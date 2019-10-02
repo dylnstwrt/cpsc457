87df3258a092f1d29c7b79e2816414a3f2a9a362
dylan.stewart <dylan.stewart@cs.ucalgary.ca>
2019-10-02 10:31:32 -0600

    commenting finished

--- A2/src/myFind.cpp
+++ A2/src/myFind.cpp
@@ -24,26 +24,36 @@ int main(int argc, char *argv[]) {
          myFind(".");
 }
 
+
 void myFind(string name) {
+    // initialize stream and dirent struct; open stream using parameter
     DIR *stream = NULL;
     struct dirent *info = NULL;
     stream = opendir(name.c_str());
 
+    //if stream is valid, populate dirent struct
     if (stream != NULL) {
         info = readdir(stream);
-        while( info != NULL) {
+        // for all valid files in the directory
+        while( info != NULL) { 
+            // if link to current/prev directory; skip
             if ((string(info->d_name) == ".") || (string(info->d_name)== "..")) {
                 info = readdir(stream);
                 continue;
             }
+            // concat name with / and the current selected item in the directory
             string fullpath = name + "/" + info->d_name;
+            // using d_type flag; check if current item is a directory and recurse if true
             if (info->d_type == DT_DIR) {
                 myFind(fullpath);
             } else {
+                // note to self, find -type f shows all files, not just regular ones like the DT_REG flag shows.
                 cout << fullpath << endl;
             }
+            // iterate to next item in directory
             info = readdir(stream);
         }
     }
+    // close stream
     closedir(stream);
 }
