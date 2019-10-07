63b5a6de20c3f693065b8af6e4006108e8957f51
dylan.stewart <dylan.stewart@cs.ucalgary.ca>
2019-10-07 11:28:16 -0600

    A2,Q5 - ints read into array; more test files, one small one large

--- A2/src/scan.cpp
+++ A2/src/scan.cpp
@@ -1,3 +1,15 @@
+/**********************************************
+ * Last Name:   Stewart
+ * First Name:  Dylan
+ * Student ID:  30024193
+ * Course:      CPSC 457
+ * Tutorial:    03
+ * Assignment:  2
+ * Question:    Q3
+ *
+ * File name: scan.cpp
+ *********************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- A2/src/sum.cpp
+++ A2/src/sum.cpp
@@ -1,3 +1,15 @@
+/**********************************************
+ * Last Name:   Stewart
+ * First Name:  Dylan
+ * Student ID:  30024193
+ * Course:      CPSC 457
+ * Tutorial:    03
+ * Assignment:  2
+ * Question:    Q5
+ *
+ * File name: sum.cpp
+ *********************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,19 +26,30 @@ const int MAX_INT_COUNT = 1000000;
 
 int main(int argc, char *argv[]) {
 
-    if (argc != 2) {
+    if (argc != 3) {
         cout << "Usage: <input file> <# of threads>" << endl;
         exit(EXIT_FAILURE);
     }
 
     int lineCount = 0;
+    int toAdd;
 
     ifstream stream;
     stream.open(argv[1]);
 
+
     char integer[BUFFER_SIZE];
     int array[MAX_INT_COUNT];
 
+      while (!stream.eof()) {
+          memset(integer, 0, 256);
+          stream.getline(integer, 256);
+          toAdd = atoi(integer);
+          array[lineCount] = toAdd;
+          lineCount = lineCount + 1; 
+      }
     stream.close();
+
+    
     return EXIT_SUCCESS;
 }
