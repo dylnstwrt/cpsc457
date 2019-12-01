baddd9ed6ff0afc7b1024ab1748d4cd54daa28ae
dylan.stewart <dylan.stewart@cs.ucalgary.ca>
2019-11-30 20:41:43 -0700

    skeletons for lru and clock algorithms.

--- A5/pagesim.cpp
+++ A5/pagesim.cpp
@@ -11,9 +11,19 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stack>
 
 std::string optimal(int, std::vector<int>);
+std::string lru(int, std::vector<int>);
+std::string clockAlg(int, std::vector<int>);
 
+/**
+ * @brief 
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
 int main(int argc, char *argv[])
 {
     if (argc != 2) 
@@ -22,11 +32,8 @@ int main(int argc, char *argv[])
         exit(-1);
     }
     
-    //bool reading = true;
     int frames = atoi(argv[1]); 
 
-   /*  while (reading)
-    { */
         std::vector<int> reference;
         int pagenum;
 
@@ -36,11 +43,20 @@ int main(int argc, char *argv[])
             reference.push_back(pagenum);
         }
 
-        std::cout << optimal(frames, reference) << "\n";
+        std::cout << optimal(frames, reference);
+        std::cout << lru(frames, reference);
+        std::cout << clockAlg(frames,reference);
     //}
     return(0);
 }
 
+/**
+ * @brief 
+ * 
+ * @param maxFrames 
+ * @param reference 
+ * @return std::string 
+ */
 std::string optimal(int maxFrames, std::vector<int> reference)
 {   
     std::string toWrite;
@@ -96,6 +112,108 @@ std::string optimal(int maxFrames, std::vector<int> reference)
                     }
                 }
             }
+            
+            frames[indexToReplace] = toInsert;
+            reference.erase(reference.begin());
+            ++faults;
+        }
+    }
+    
+    toWrite += "\nOptimal:\n\t- frames:";
+    for (auto page : frames)
+    {
+        toWrite += " " + std::to_string(page);
+    }
+    toWrite += "\n\t- page faults: " + std::to_string(faults);
+    return toWrite;
+}
+
+/**
+ * @brief TODO counter based version of the lru implementation.
+ * 
+ * @param maxFrames 
+ * @param reference 
+ * @return std::string 
+ */
+std::string lru(int maxFrames, std::vector<int> reference)
+{
+    std::string toWrite;
+    std::vector<int> frames(maxFrames, -1);
+    std::stack<int> pagesUsed;
+    int faults = 0;
+    
+    while(!reference.empty())
+    {
+        int toInsert = reference[0];
+        
+        if (std::find(frames.begin(), frames.end(), toInsert) != frames.end())
+        {
+            reference.erase(reference.begin());
+            continue;
+        }
+        else
+        {   
+            int indexToReplace = -1;
+            
+            for (int i = 0; i < frames.size(); i++)
+            {
+                if (frames[i] == -1)
+                {
+                    indexToReplace = i;
+                    break;
+                }
+            }
+            
+            if (indexToReplace == -1)
+            {
+                
+            }
+            
+            frames[indexToReplace] = toInsert;
+            reference.erase(reference.begin());
+            ++faults;
+        }
+    }
+    
+    toWrite += "\nOptimal:\n\t- frames:";
+    for (auto page : frames)
+    {
+        toWrite += " " + std::to_string(page);
+    }
+    toWrite += "\n\t- page faults: " + std::to_string(faults);
+    return toWrite;
+}
+
+std::string clockAlg(int maxFrames, std::vector<int> reference)
+{
+    std::string toWrite;
+    std::vector<int> frames(maxFrames, -1);
+    int faults = 0;
+    
+    while(!reference.empty())
+    {
+        int toInsert = reference[0];
+        
+        if (std::find(frames.begin(), frames.end(), toInsert) != frames.end())
+        {
+            reference.erase(reference.begin());
+            continue;
+        }
+        else
+        {   
+            int indexToReplace = -1;
+            
+            for (int i = 0; i < frames.size(); i++)
+            {
+                if (frames[i] == -1)
+                {
+                    indexToReplace = i;
+                    break;
+                }
+            }
+            
+            
+            
             
             frames[indexToReplace] = toInsert;
             reference.erase(reference.begin());
